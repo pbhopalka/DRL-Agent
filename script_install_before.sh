31f5446ce95cb6953a57c56cf2a69f516c857265
Mahesh Uligade <maheshuligade@gmail.com>
2017-06-29 15:19:06 +0530

    Fixed Permissions Probelms
    There was problem of permissions when installing this script.

--- script_install_before.sh
+++ script_install_before.sh
@@ -1,11 +1,11 @@
-pip install --upgrade pip
-pip install --upgrade tensorflow
-pip install gym
+sudo pip install --upgrade pip
+sudo pip install --upgrade tensorflow
+sudo pip install gym
 sudo apt-get install libjpeg-turbo8-dev cmake make golang zlib1g-dev
-pip install gym[atari]
-pip install opencv-python
-git clone https://github.com/openai/universe.git
+sudo pip install gym[atari]
+sudo pip install opencv-python
+sudo git clone https://github.com/openai/universe.git
 cd universe
-pip install -e .
+sudo pip install -e .
 cd ..
-
+sudo rm -r universe
